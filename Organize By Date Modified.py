d6418f05eed637d4a5cc02df24b58ed90f7e0b7f
jhbvergara <jhbv.dev@gmail.com>
2020-09-14 00:43:28 +0800

    Added function to show sizes of OBDM folders

--- Organize By Date Modified.py
+++ Organize By Date Modified.py
@@ -6,7 +6,7 @@ curDir = os.getcwd()
 cycle = 1
 
 while cycle == 1:
-    action = input('Sort - s, Extract - e, Quit - q: ')
+    action = input('Simple File Arranger \ns - Sort \ne - Extract \ng - Get folder sizes \nq - Quit: ')
     if action == 's':
         for a in os.listdir(curDir):
             if a[-5:] != ' OBDM' and a != 'Organize By Date Modified.py':
@@ -32,6 +32,33 @@ while cycle == 1:
                     subCurFileAndDir = os.path.join(subCurDir, i)
                     shutil.move(subCurFileAndDir, curDir)
                     print('<-- ' + i)
+    elif action == 'g':
+        print('\nGetting folder sizes...')
+        folderName = []
+        folderSize = []
+
+        for a in os.listdir(curDir):
+            if a[-5:] == ' OBDM':
+                subCurDir = os.path.join(curDir, a)
+                fileSize = 0
+
+                for path, name, files in os.walk(subCurDir):
+                    for i in files:
+                        fileSize += os.path.getsize(os.path.join(path, i))
+                
+                folderName.append(subCurDir)
+                folderSize.append(fileSize)
+
+        #Arrange from highest to lowest
+        for b in range(0, len(folderSize)):
+            for i in range(b + 1, len(folderSize)):
+                if folderSize[b] < folderSize[i]:
+                    folderName[b], folderName[i] = folderName[i], folderName[b]
+                    folderSize[b], folderSize[i] = folderSize[i], folderSize[b]
+
+        for x in range(0, len(folderSize)):
+            print(folderSize[x], ' == ', folderName[x])
+
     elif action == 'q':
         cycle = 0
     else:
